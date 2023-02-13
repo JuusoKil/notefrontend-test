6a817d4c159dc839fee601e023e9d4b4c85353b2
Juuso Kilponen <juuso.kilponen@hotmail.com>
2023-02-13 20:45:23 +0200

    Trying again

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+    const footerStyle = {
+        color: 'green',
+        fontStyle: 'italic',
+        fontSize: 16
+    }
+
+    return (
+        <div style={footerStyle}>
+            <br />
+            <em>Note app, My FullStack web development 2023 course learning app</em>
+        </div>
+    )
+}
+
+export default Footer;
